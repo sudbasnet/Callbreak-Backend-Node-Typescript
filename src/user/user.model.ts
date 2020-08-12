53af4fb7cbca66f4e4956e021be2debbcd275170
Sudeep Basnet <sudbasnet10@gmail.com>
2020-08-12 18:32:56 -0500

    added a role enum for future use:

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -15,12 +15,14 @@ export interface UserSchema extends Document {
     passwordReset?: Token
 };
 
+export enum UserRole { ADMIN = 'admin', PLAYER = 'player' };
+
 const User: Schema = new Schema({
     name: { type: String, required: true, min: 3, max: 225 },
     email: { type: String, required: true, min: 6, max: 225 },
     password: { type: String, required: true, max: 1024, min: 6 },
     active: { type: Boolean, default: false },
-    role: { type: String, default: 'PLAYER' },
+    role: { type: String, default: UserRole.PLAYER },
     verification: {
         type: { token: { type: String }, expires: { type: Date } }
     },
