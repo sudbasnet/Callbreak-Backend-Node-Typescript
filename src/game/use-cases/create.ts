2796422935127c6235317e409ffc57a85b8addbf
Sudeep Basnet <sudbasnet10@gmail.com>
2020-08-27 19:26:51 -0500

    corrected response to create-game

--- src/game/use-cases/create.ts
+++ src/game/use-cases/create.ts
@@ -5,10 +5,10 @@ import CustomError from '../../_helpers/custom-error';
 
 const create: RequestHandler = async (req, res, next) => {
     const userId = req.userId;
+    const userName = req.userName;
     const gameType = req.params.gameType;
 
     try {
-
         const user = await User.findById(userId);
         if (!user) {
             throw new CustomError('Invalid Request', 404);
@@ -17,39 +17,39 @@ const create: RequestHandler = async (req, res, next) => {
         // if the player has already created a game, then return the existing game
         const incompleteGame = await Game.findOne({ createdBy: userId, status: { $ne: gameStatus.COMPLETE } });
         if (incompleteGame) {
-            res.status(200).json({ global: incompleteGame.global, players: incompleteGame.players });
+            res.status(200).json({ gameType: gameType, gameId: incompleteGame._id });
+        } else {
+            const game = new Game({
+                global: {
+                    gameNumber: 0,
+                    roundNumber: 0,
+                    turnNumber: 0,
+                    scores: [],
+                    bets: [],
+                    playedRounds: [],
+                    currentTurn: userId,
+                    nextTurn: null,
+                    currentSuit: null,
+                    overriddenBySpades: false
+                },
+                players:
+                    [
+                        {
+                            playerId: userId,
+                            playerName: userName,
+                            cards: [],
+                            possibleMoves: []
+                        }
+                    ],
+
+                status: gameStatus.WAITING,
+                createdBy: userId,
+                gameType: gameType,
+                start: new Date()
+            });
+            const savedGame = await game.save();
+            res.status(200).json({ gameType: gameType, gameId: savedGame._id });
         }
-
-        const game = new Game({
-            global: {
-                gameNumber: 0,
-                roundNumber: 0,
-                turnNumber: 0,
-                scores: [],
-                bets: [],
-                playedRounds: [],
-                currentTurn: userId,
-                nextTurn: null,
-                currentSuit: null,
-                overriddenBySpades: false
-            },
-            players:
-                [
-                    {
-                        playerId: userId,
-                        playerName: 'DummyName',
-                        cards: [],
-                        possibleMoves: []
-                    }
-                ],
-
-            status: gameStatus.WAITING,
-            createdBy: userId,
-            gameType: gameType,
-            start: new Date()
-        });
-        const savedGame = await game.save();
-        res.status(200).json({ gameType: gameType, gameId: savedGame._id });
     } catch (err) {
         next(err);
     }
