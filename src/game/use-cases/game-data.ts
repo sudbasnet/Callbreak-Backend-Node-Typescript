101687084e5bf377cf7a0fbc5939a9564d929a45
sudbasnet <sudbasnet10@gmail.com>
2020-09-26 23:49:56 -0500

    return possible moves to player

--- src/game/use-cases/game-data.ts
+++ src/game/use-cases/game-data.ts
@@ -3,6 +3,24 @@ import { RequestHandler } from 'express';
 import User from '../../user/user.model';
 import CustomError from '../../_helpers/custom-error';
 import gameResponse from '../../_helpers/game-response';
+import Deck, { Card, suits } from '../../_entities/Deck';
+
+const getPossibleMoves = (cards: Card[], currentWinner: Card | undefined, currentSuit: suits | undefined) => {
+    const possibleMoves: Card[] = [];
+    if (cards && currentWinner && currentSuit) {
+        let winner: Card;
+        cards.forEach(card => {
+            winner = Deck.calculateCallbreakWinner(currentWinner, card, currentSuit);
+            if (currentWinner.suit != winner.suit || currentWinner.value != winner.value) {
+                possibleMoves.push(winner);
+            }
+        });
+    } else {
+        return cards;
+    }
+    return possibleMoves;
+}
+
 
 const gameData: RequestHandler = async (req, res, next) => {
     const userId = req.userId;
@@ -15,12 +33,22 @@ const gameData: RequestHandler = async (req, res, next) => {
             throw new CustomError('Invalid Request', 404);
         }
 
-        let incompleteGame: GameSchema;
-        const activeGames = await Game.find({ status: { $ne: gameStatus.INACTIVE } });
-        const incompleteGames = activeGames.filter(g => g.privatePlayerList.map(p => String(p.id)).includes(userId));
+        let game: GameSchema;
+        const allActiveGames = await Game.find({ status: { $ne: gameStatus.INACTIVE } });
+        const incompleteGames = allActiveGames.filter(g => g.playerList.map(p => String(p.id)).includes(userId));
         if (incompleteGames.length > 0) {
-            incompleteGame = incompleteGames[0];
-            res.status(200).json(gameResponse(userId, incompleteGame));
+            game = incompleteGames[0];
+
+            if (String(game.currentTurn) === userId) {
+                const index = game.privatePlayerList.findIndex(p => String(p.id) === userId);
+                const privatePlayer = game.privatePlayerList[index];
+                game.privatePlayerList[index].possibleMoves = getPossibleMoves(privatePlayer.cards, game.currentWinningCard, game.currentSuit);
+                const savedGame = await game.save();
+
+                res.status(200).json(gameResponse(userId, savedGame));
+            }
+
+            res.status(200).json(gameResponse(userId, game));
         } else {
             throw new CustomError('The current user has no pre-existing game', 404);
         }
