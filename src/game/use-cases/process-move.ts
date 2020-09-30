99a79f991f7c1b20ce434f3f4510e5e28c8a8fa7
sudbasnet <sudbasnet10@gmail.com>
2020-09-30 10:08:15 -0500

    added check to ensure totalScore is only 2 decimal places

--- src/game/use-cases/process-move.ts
+++ src/game/use-cases/process-move.ts
@@ -53,7 +53,9 @@ const placeCard: RequestHandler = async (req, res, next) => {
 
             if (game.playerList[handWinnnerIndex].score >= game.playerList[handWinnnerIndex].bet) {
                 game.playerList[handWinnnerIndex].ots += 1;
+                console.log(`before: ${game.playerList[handWinnnerIndex].totalScore}`);
                 game.playerList[handWinnnerIndex].totalScore += 0.1;
+                game.playerList[handWinnnerIndex].totalScore = Math.round(game.playerList[handWinnnerIndex].totalScore * 10) / 10; //some problem with js addition
             } else {
                 game.playerList[handWinnnerIndex].score += 1;
                 game.playerList[handWinnnerIndex].totalScore += 1;
